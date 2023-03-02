31fe28670d34865e80673cb55cebcba333a6724a
mahanteshm94 <mahantesh0917@gmail.com>
2023-03-02 09:05:38 +0530

    msg

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner s1=new Scanner(System.in);
+    System.out.println("enter intiger value of a");
+
+    int a=s1.nextInt();
+    
+    System.out.println("enter intiger valueof b");
+
+    int b=s1.nextInt();
+
+        System.out.println(a+b);
+
+  }
+}
